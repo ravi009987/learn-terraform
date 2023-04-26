27f53b42c9b51034748dbea6d6f26119ea21276c
Ravi <rp97.reddy@gmail.com>
2023-04-26 22:18:36 +0530

    adding changes

--- vars/outputs.tf
+++ vars/outputs.tf
@@ -1,3 +1,7 @@
 output "sample_string" {
   value = var.sample_string
 }
+
+output "sample_list_2" {
+  value = var.sample_list[1]
+}
--- vars/vars.tf
+++ vars/vars.tf
@@ -1,3 +1,19 @@
 variable "sample_string" {
   default = "hello world"
 }
+
+variable "sample_number" {
+  default = 100
+}
+
+variable "sample_boolean" {
+  default = true
+}
+
+variable "sample_list" {
+  default = [
+    100,
+    "hello world",
+    false
+  ]
+}
